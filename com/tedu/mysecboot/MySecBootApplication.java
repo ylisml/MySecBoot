9a5bb02bb3ce5f8ed9e21ab3f4171aba3a102f97
yanghongtao <2460374290@qq.com>
2022-08-25 15:15:03 +0800

    Initial commit

---
+++ com/tedu/mysecboot/MySecBootApplication.java
@@ -0,0 +1,13 @@
+package com.tedu.mysecboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySecBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySecBootApplication.class, args);
+    }
+
+}
